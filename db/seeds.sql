03f75188b8dacca0a67495cd6226ecd5ffdbdc9f
Kristen Kirchgesler <kristen.arbutiski@gmail.com>
2018-05-10 21:21:59 -0400

    setting up folders

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Basic Burger - tomato, lettuce, pickles');
+INSERT INTO burgers (burger_name) VALUES ('Faux Big Mac - two beef patties, special sauce, pickles, chopped onion');
+INSERT INTO burgers (burger_name, devoured) VALUES ('turkey burger, avocado, Monterey Jack cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('black bean patty, microgreens, special spice aoli', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cholesterol Lovers Burger - cheddar, fried egg, bacon', true);
