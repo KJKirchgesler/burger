329c0ecb735d261deda7c86f10ce9c79243d76ca
Kristen Kirchgesler <kristen.arbutiski@gmail.com>
2018-06-03 15:18:36 -0400

    added Timestamp

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ('Basic Burger - tomato, lettuce, pickles');
 INSERT INTO burgers (burger_name) VALUES ('Faux Big Mac - two beef patties, special sauce, pickles, chopped onion');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey burger - avocado, Monterey Jack cheese', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean burger - microgreens, special spice aoli', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cholesterol Lovers Burger - cheddar, fried egg, bacon', true);
+INSERT INTO burgers (burger_name) VALUES ('Turkey burger - avocado, Monterey Jack cheese', true);
+INSERT INTO burgers (burger_name) VALUES ('Black Bean burger - microgreens, special spice aoli', true);
+INSERT INTO burgers (burger_name) VALUES ('Cholesterol Lovers Burger - cheddar, fried egg, bacon', true);
