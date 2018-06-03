f2908c60277d08b187e051d106ec4e9b4c2f5a79
Kristen Kirchgesler <kristen.arbutiski@gmail.com>
2018-06-03 12:17:06 -0400

    fixed handlebars and added method-override code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 // Create instance of express app.
 var app = express();
@@ -8,6 +9,25 @@ var app = express();
 // Set the port of the application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
 
 
 // Start server to listen for client requests.
